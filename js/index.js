5f9ceced629b05f081632af08667cc251e7179bb
School Of Code <isabellaRebecca@live.co.uk>
2023-08-16 04:52:26 +0100

    needs further refinement needed, core functionality and layout done.

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// slideshow
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const slides = document.querySelector(".slideshow-container");
+const track = document.querySelector(".track");
+
+let width = slides.offsetWidth;
+let i = 0;
+
+window.addEventListener("resize", function () {
+  width = slides.offsetWidth;
+});
+
+next.addEventListener("click", function (e) {
+  e.preventDefault();
+  i = i + 1;
+  track.style.transform = "translateX(" + i * -width + "px)";
+});
+
+prev.addEventListener("click", function () {
+  i = i - 1;
+  track.style.transform = "translateX(" + i * -width + "px)";
+});
+
